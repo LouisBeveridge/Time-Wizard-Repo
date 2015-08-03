b7c3e1020c1b35eb2303c79ed0a51de41720751d
ProfessorHoover <andrew.parnell@connect.qut.edu.au>
2015-08-04 07:30:12 +1000

    Menu and Tutorial valley
    Menu has camera issues and tute is missing water, dogs and tooltips

---
+++ Time Wizard 380/Assets/Scripts/MenuNewGame.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuNewGame : MonoBehaviour {
+	public Renderer rend;
+	//Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		}
+	void OnMouseEnter ()
+		{
+			rend.material.color = Color.grey;
+		}
+	void OnMouseExit ()
+		{
+			rend.material.color = Color.white;
+		}
+}
---
+++ Time Wizard 380/Assets/Scripts/MenuScreen.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuScreen : MonoBehaviour {
+
+	public Canvas Menu;
+	public Button NewGame;
+	public Button Quit;
+	public Button LevelSelect;
+
+	// Use this for initialization
+	void Start () {
+		Quit = Quit.GetComponent <Button> ();
+		NewGame = NewGame.GetComponent <Button> ();
+		LevelSelect = LevelSelect.GetComponent <Button> ();
+		Menu.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PressQuit()
+	{
+		Application.Quit ();
+	}
+
+	public void StartGame ()
+	{
+		Menu.enabled = false;
+		Application.LoadLevel (1);
+	}
+}
